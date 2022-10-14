3ce9a2de511c1813400c729c17bea8e18682ff70
VGEEK BRANCH <vgeekbranch@MacBook-Pro-VGEEK.local>
2022-10-14 21:26:27 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+Console.Write("Введите значение А: ");
+int userNumA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение B: ");
+int userNumB = Convert.ToInt32(Console.ReadLine());
+getNumNaturDegree(userNumA, userNumB);
+
+void getNumNaturDegree(int numA, int numB)
+{
+    int result = 1; 
+    for(int i = 1; i <= numB; i++) 
+    {
+        result = result * numA; 
+    }
+    Console.WriteLine($"Возведение {userNumA} в натуральную степень {userNumB} равна {result}");
+}
